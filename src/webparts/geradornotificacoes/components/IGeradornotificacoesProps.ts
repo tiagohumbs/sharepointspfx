69ab01c05e9f8ae887a45a9b0a426a2897c7e4f3
Tiago Humberto Silva <tiagohumbs@gmail.com>
2024-06-12 17:45:23 +0000

    Criação projeto

---
+++ src/webparts/geradornotificacoes/components/IGeradornotificacoesProps.ts
@@ -0,0 +1,7 @@
+export interface IGeradornotificacoesProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/geradornotificacoes/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IGeradornotificacoesWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'GeradornotificacoesWebPartStrings' {
+  const strings: IGeradornotificacoesWebPartStrings;
+  export = strings;
+}
